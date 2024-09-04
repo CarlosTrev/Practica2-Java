bf3e4cccfa211e40fcef2b2bba2b4f3adb854d15
CarlosTrev <alu.21130838@correo.itlalaguna.edu.mx>
2024-09-04 16:00:39 -0600

    Se agreagaron las funcionalidades principales del proyecto

---
+++ src/Practica2/Pantalla1.java
@@ -0,0 +1,72 @@
+package Practica2;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Pantalla1 {
+    private JTextField txtfldnum1;
+    private JTextField txtfldnum2;
+    private JButton btnsumar;
+    private JButton btnrestar;
+    private JButton btnmultiplicar;
+    private JButton btndividir;
+    private JButton btnborrar;
+    private JPanel PanelPantalla1;
+
+
+    public Pantalla1() {
+        btnsumar.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                double num1 = Integer.parseInt(txtfldnum1.getText());
+                double num2 = Integer.parseInt(txtfldnum2.getText());
+                JOptionPane.showMessageDialog(null, "El resultado es: " + (num1+ num2));
+            }
+        });
+        btnborrar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                txtfldnum1.setText("");
+                txtfldnum2.setText("");
+            }
+        });
+        btnrestar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                double num1 = Integer.parseInt(txtfldnum1.getText());
+                double num2 = Integer.parseInt(txtfldnum2.getText());
+                JOptionPane.showMessageDialog(null, "El resultado es: " + (num1 - num2));
+            }
+        });
+        btnmultiplicar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                double num1 = Integer.parseInt(txtfldnum1.getText());
+                double num2 = Integer.parseInt(txtfldnum2.getText());
+                JOptionPane.showMessageDialog(null, "El resultado es: " + (num1 * num2));
+            }
+        });
+        btndividir.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                double num1 = Integer.parseInt(txtfldnum1.getText());
+                double num2 = Integer.parseInt(txtfldnum2.getText());
+                JOptionPane.showMessageDialog(null, "El resultado es: " + (num1 / num2));
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Pantalla de operaciones");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500, 300); //Tamaño de la ventana
+        frame.setContentPane(new Pantalla1().PanelPantalla1);
+        frame.setBounds(30,30,700,700);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
+
+
